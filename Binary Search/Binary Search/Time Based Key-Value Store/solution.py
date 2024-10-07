99db4441a6411ddae438258b96cdffc71d5a8010
sally0127 <yushandai9@gmail.com>
2024-10-07 10:15:16 +0800

    Update solution.py

--- Binary Search/Binary Search/Time Based Key-Value Store/solution.py
+++ Binary Search/Binary Search/Time Based Key-Value Store/solution.py
@@ -14,18 +14,20 @@ class TimeMap:
         if key not in self.store:
             return""
 
-        # Get the list of (timestamp, value) pairs for the key
+        # Get the list of (timestamp, value) pairs for the key          **首先取得對應 key 的時間戳和值的列表，這個列表中的每個元素是 (timestamp, value) 的形式，並且時間戳是嚴格遞增的。**
         values = self.store[key]
 
 
-        # Binary search to find the rightmost value with timestamp <= given timestamp
-        idx = bisect.bisect_right(values,(timestamp,chr(255)))
+        # Binary search to find the rightmost value with timestamp <= given timestamp   **bisect_right 是 Python 的 bisect 模塊中的一個方法，適用於二分查找。**                                                                                        
+        idx = bisect.bisect_right(values,(timestamp,chr(255)))                          **(timestamp, chr(255)) 是用來比較的假設值：這裡 chr(255) 是一個高 ASCII 字符（最接近 255 的字符），其主要目的是確保當遇到相同 timestamp 時，這個值會放在最後。因為 values 中的 (timestamp, value) 是按照時間戳排序的。
+                                                                                        **最終，bisect_right 會返回一個插入點索引 idx，表示找到的索引 idx 是第一個大於 (timestamp, chr(255)) 的位置，這樣可以有效找到 timestamp <= 給定的 timestamp 的所有符合條件的最後一個位置。**
 
 
-        if idx == 0:
+        if idx == 0:                                                        **當 idx == 0 時，說明所有的時間戳都比給定的 timestamp 大，找不到符合條件的值，因此返回空字串 **
             return ""
 
-        return values[idx - 1][1]
+        return values[idx - 1][1]                                            **values[idx - 1] 給出的是 (timestamp, value)，也就是某一個符合條件的時間戳和它的值。**
+                                                                             **[1] 表示從這個元組中取出第二個元素，即 value**
 
 
 ================================================================================
