78e28f6a4f5c6774bc1e1814392b3898e869f760
sally0127 <yushandai9@gmail.com>
2024-10-08 14:27:50 +0800

    Update solution.py

--- Sliding Window/Buy and Sell Crypto/solution.py
+++ Sliding Window/Buy and Sell Crypto/solution.py
@@ -1,1 +1,13 @@
+from typing import List
+class Solution:
+    def maxProfit(self, prices: List[int]) -> int:
+        min_price = float('inf')
+        max_profit = 0
 
+        for price in prices:
+            min_price = min(min_price,price)
+            potential_profit = price - min_price
+            max_profit = max(max_profit,potential_profit)
+
+
+        return max_profit
